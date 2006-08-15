bc4b8ccce41244b31f544a0fb5e0baa4995ec09b
schapht <schapht@98384642-6f1a-0410-826c-b3bf4801d319>
2006-08-15 14:56:29 +0000

    Got the animations and everything more or less working.  Still one bug to pin down where the text isn't in the right place after the resize.
    git-svn-id: https://dwc.svn.sourceforge.net/svnroot/dwc/Digital World Clock/src/trunk@31 98384642-6f1a-0410-826c-b3bf4801d319

--- Digital World Clock.wdgt/Flipper.js
+++ Digital World Clock.wdgt/Flipper.js
@@ -40,120 +40,66 @@ function hidePreferences()
 	   front.style.display=frontDisplay;	// show the front
     });	
     exitflip();     //Hide the "i" incase the mouse moved during animation
-    mouseexit();
 }
 
-// Code below adapted from Apple's developer guides
-var flipShown = new Object;
-var animation = {duration:0, starttime:0, to:1.0, now:0.0, from:0.0, firstElement:null, timer:null};
-
-// mousemove() is the event handle assigned to the onmousemove property on the front div of the widget. 
-// It is triggered whenever a mouse is moved within the bounds of your widget.  It prepares the
-// preference flipper fade and then calls animate() to performs the animation.
-
+/**
+ * Handles display actions when mouse is over the widget.  Namely, displaying the front-side controls.
+ */
 function mousemove (event)
 {
     fadeIn(document.getElementById ('flip'));
     fadeIn(document.getElementById ('resize'));
 }
 
-// mouseexit() is the opposite of mousemove() in that it preps the preferences flipper
-// to disappear.  It adds the appropriate values to the animation data structure and sets the animation in motion.
-
+/**
+ * Handles display actions when mouse leaves the widget.  Namely, hiding the front-side controls.
+ */
 function mouseexit (event)
 {
-    fadeOut(document.getElementById ('flip'));
-    fadeOut(document.getElementById ('resize'));
+    if (event.toElement && (event.toElement.nodeName == 'BODY')) {
+        fadeOut(document.getElementById ('flip'));
+        fadeOut(document.getElementById ('resize'));
+    }
+}
+
+var itemsShown = new Object;
+
+function fade(item, direction) {
+    itemsShown[item.id] = !itemsShown[item.id]; 
+    animator = new AppleAnimator(500, 13);
+  
+    if (direction == "in") {
+      animation = new AppleAnimation(0.0, 1.0, animationHandler(item));
+    } else {
+      animation = new AppleAnimation(1.0, 0.0, animationHandler(item));
+    }
+    animation.item = item;
+    
+    animator.addAnimation(animation);
+    
+    animator.start();
 }
 
 function fadeIn(item) {
-	if (!flipShown[item.id])			// if the preferences flipper is not already showing...
-	{
-	    
-		if (animation.timer != null)			// reset the animation timer value, in case a value was left behind
-		{
-			clearInterval (animation.timer);
-			animation.timer  = null;
-		}
-		
-		var starttime = (new Date).getTime() - 13; 		// set it back one frame
-		
-		animation.duration = 500;												// animation time, in ms
-		animation.starttime = starttime;										// specify the start time
-		animation.firstElement = item;		// specify the element to fade
-		animation.timer = setInterval ("animate();", 13);						// set the animation function
-		animation.from = animation.now;											// beginning opacity (not ness. 0)
-		animation.to = 1.0;														// final opacity
-		animate();																// begin animation
-		flipShown[item.id] = true;														// mark the flipper as animated
-	}
+    if (!itemsShown[item.id]) {
+        fade(item, "in");
+    }
 }
 
 function fadeOut(item) {
-	if (flipShown[item.id])
-	{
-		// fade in the flip widget
-		if (animation.timer != null)
-		{
-			clearInterval (animation.timer);
-			animation.timer  = null;
-		}
-		
-		var starttime = (new Date).getTime() - 13;
-		
-		animation.duration = 500;
-		animation.starttime = starttime;
-		animation.firstElement = item;
-		animation.timer = setInterval ("animate();", 13);
-		animation.from = animation.now;
-		animation.to = 0.0;
-		animate();
-		flipShown[item.id] = false;
-	}
+    if (itemsShown[item.id]) {
+        fade(item, "out");
+    }
 }
 
-
-// animate() performs the fade animation for the preferences flipper. It uses the opacity CSS property to simulate a fade.
-
-function animate()
+function animationHandler(item)
 {
-	var T;
-	var ease;
-	var time = (new Date).getTime();
-		
-	
-	T = limit_3(time-animation.starttime, 0, animation.duration);
-	
-	if (T >= animation.duration)
-	{
-		clearInterval (animation.timer);
-		animation.timer = null;
-		animation.now = animation.to;
-	}
-	else
-	{
-		ease = 0.5 - (0.5 * Math.cos(Math.PI * T / animation.duration));
-		animation.now = computeNextFloat (animation.from, animation.to, ease);
-	}
-	
-	animation.firstElement.style.opacity = animation.now;
+    return function(animation, current, start, finish) {
+        item.style.opacity = current;
+    }
 }
 
-
-// these functions are utilities used by animate()
-
-function limit_3 (a, b, c)
-{
-    return a < b ? b : (a > c ? c : a);
-}
-
-function computeNextFloat (from, to, ease)
-{
-    return from + (to - from) * ease;
-}
-
-// Provides hover function
-
+// Provides hover functionality on the "i" circular border
 function enterflip()
 {
 	document.getElementById('fliprollie').style.display = 'block';
