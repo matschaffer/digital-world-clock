55ef0b4b9b42186bf5b5cbccb6cd3fd81bea8de9
schapht <schapht@98384642-6f1a-0410-826c-b3bf4801d319>
2006-08-18 02:48:04 +0000

    Cleaned up some code and added a bunch of comments.
    git-svn-id: https://dwc.svn.sourceforge.net/svnroot/dwc/Digital World Clock/src/trunk@37 98384642-6f1a-0410-826c-b3bf4801d319

--- Digital World Clock.wdgt/DSTCalculatorInit.js
+++ Digital World Clock.wdgt/DSTCalculatorInit.js
@@ -2,37 +2,46 @@
  * Defines DSTCalculator and a series of functions for easing DST calculations.
  */
 
-DSTCalculator = new Object;
-DSTCacher = new Object;
+DSTCalculator = {};
+DSTCacher = {};
 
 /**
- * Retrieves and caches the current day of the month.
+ * Retrieves and caches the date for the given calibration value of this year.  Once the calibration value is correct
+ * for one year, it will yield the n-th weekday of a given month in any given year.
+ * @param String cacheKey The key to be used when looking up values in the cache (usually a DST rule name).
+ * @param int dayOffset The number of days to offset the calculation function to yield the correct date.
+ * @param int calibrate The calibration number to yield the n-th weekday of a given month.
  */
 function getDayOfMonth(cacheKey, dayOffset, calibrate) {
     var year = (new Date().getUTCFullYear()); 
+    cache = DSTCacher[cacheKey];
     
-    if (!DSTCacher[cacheKey]) {
-        DSTCacher[cacheKey] = new Object;
-        DSTCacher[cacheKey]['year'] = year;
-        DSTCacher[cacheKey]['result'] = calculateDayOfMonth(year, dayOffset, calibrate);
+    if (!cache) {
+        cache = {};
+        cache['year'] = year;
+        cache['result'] = calculateDayOfMonth(year, dayOffset, calibrate);
     } else {
-        if (year != DSTCacher[cacheKey]['year']) {
-            DSTCacher[cacheKey]['result'] = calculateDayOfMonth(year, dayOffset, calibrate);
+        if (year != cache['year']) {
+            cache['result'] = calculateDayOfMonth(year, dayOffset, calibrate);
         }
     }
 
-    return DSTCacher[cacheKey]['result'];
+    return cache['result'];
 }
 
 /**
- * 
+ * Yields the appropriate date for the n-th weekday of a given month.  This is still voodoo to me.  But once you find the
+ * corrent dayOffset and calibrate values for a given month and year, it will work for that month in any given year.
+ * @param int year The year to calculate the date for.
+ * @param int dayOffset The number of days to offse the calculation (usually 1..7 or 24..31 to indicate the first or last week of the month).
+ * @param int calibrate An integer that when correct yields the approprate month.  No correlation yet observed.
  */
 function calculateDayOfMonth(year, dayOffset, calibrate) {
     return dayOffset - ((Math.floor((5 * year) / 4) + calibrate) % 7);
 }
 
 /**
- *
+ * 
  */
 function givenAreaIsInOffset(name, startMonth, endMonth, startDayOffset, startDayCalibrate, endDayOffset, endDayCalibrate, changeTime, offsetFromGMT) {
     now = new Date();
@@ -43,6 +52,13 @@ function givenAreaIsInOffset(name, startMonth, endMonth, startDayOffset, startDa
 
 /**
  * Checks to see if a given time is in DST offset based on start/end time
+ * @param Date now The current date/time
+ * @param int startMonth The month (1..12) DST starts.
+ * @param int endMonth The month (1..12) DST ends.
+ * @param int startDay The day (1..12) DST starts.
+ * @param int endDay The day (1..12) DST ends.
+ * @param int offsetFromGMT The offset from GMT for the zone in question (in hours).
+ * @param int changeTime The hour (00..23) when the DST change occurs.
  */
 function offsetTest(now, startMonth, endMonth, startDay, endDay, offsetFromGMT, changeTime) {
 	var month = now.getUTCMonth() + 1;
